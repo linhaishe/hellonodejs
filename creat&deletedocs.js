d967f11462e74693d93fffa3eb417731df0846c9
linhaishe <xusumu@gmail.com>
2019-07-13 15:55:47 +0800

    addnotes

--- creat&deletedocs.js
+++ creat&deletedocs.js
@@ -2,11 +2,11 @@ var fs = require('fs');
 //操作文件系统
 
 //创建目录
-fs.mkdir('stuff', function() {
+fs.mkdir('stuff', function () {
     //将当前路径下的文件读取出来，编码，
-    fs.readFile('readMe.txt', 'utf8', function(err, data) {
+    fs.readFile('readMe.txt', 'utf8', function (err, data) {
         //创建目录和文件，在新建的stuff目录下，改名为writeme,输入读取的文件的内容
-        fs.writeFile('./stuff/writeMe.txt', data, function() {
+        fs.writeFile('./stuff/writeMe.txt', data, function () {
             console.log('copy successfully');
         })
     })
@@ -21,3 +21,37 @@ fs.mkdir('stuff', function() {
 //fs.mkdirsync("stuff");
 //删除目录
 //fs.rmdirsync("stuff");
+
+// const fs = require('fs');
+// const mkdir = function (dirname) {
+//     return new Promise(function (resolve, reject) {
+//         fs.mkdir(dirname, () => {
+//             resolve();
+//         });
+//     });
+// };
+// const readFile = function (fileName) {
+//     return new Promise(function (resolve, reject) {
+//         fs.readFile(fileName,
+//             function (error, data) {
+//                 if (error) return reject(error);
+//                 resolve(data);
+//             });
+//     });
+// };
+// const writeFile = function (fileName, data) {
+//     return new Promise(function (resolve, reject) {
+//         fs.writeFile(fileName, data, () => {
+//             resolve(1);
+//         })
+//     });
+// };
+// const asyncReadFile = async
+// function() {
+//     const f1 = await mkdir('stuff');
+//     const f2 = await readFile('./abc.txt');
+//     const f3 = await writeFile('./stuff/abc_copy.txt', f2);
+// };
+// asyncReadFile().then((d) => {
+//     console.log('copy success');
+// })
